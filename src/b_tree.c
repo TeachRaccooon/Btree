e45ade98afc7d7b71da299fde034ffb9a41597ea
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-04 14:01:40 -0400

    1

--- src/b_tree.c
+++ src/b_tree.c
@@ -343,6 +343,7 @@ unsigned int b_tree_insert(void *b_tree, void *key, void *record)
 
       // Search for a place in the found node to insert the key
       int i = 0;
+      printf("KEY IS %d\n", *(int*) key);
       while(memcmp(key, node_found->keys[i], mytree->key_size) > 0 && node_found->keys[i] != 0)
       {
          printf("Key at %d, %d\n", i, (int) *(node_found->keys[i]));
