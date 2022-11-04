826f8d7fb1cb18e1cb4737328dfb55a3e48d7ece
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-04 13:15:43 -0400

    1

--- src/b_tree.c
+++ src/b_tree.c
@@ -337,6 +337,7 @@ unsigned int b_tree_insert(void *b_tree, void *key, void *record)
       int i = 0;
       while(memcmp(key, node_found->keys[i], mytree->key_size) > 0)
       {
+         fprintf(stderr, "%d", i);
          ++i;
       }
       // shift all keys to the right by one 
