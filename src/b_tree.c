bf683d32c33bb6210da2b1a602d20dc4d15c8d97
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-05 14:57:09 -0400

    2

--- src/b_tree.c
+++ src/b_tree.c
@@ -362,7 +362,7 @@ unsigned int b_tree_insert(void *b_tree, void *key, void *record)
    printf("In Insert\n");
  
    B_Tree* mytree = (B_Tree*) b_tree;
-   //b_tree_print_tree(mytree);
+   b_tree_print_tree(mytree);
 
    int lba = b_tree_find(b_tree, key);
 
