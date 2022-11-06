4298b9a92b13046888b32deef131853d25af21ca
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-06 13:14:52 -0500

    3

--- src/b_tree.c
+++ src/b_tree.c
@@ -268,7 +268,9 @@ unsigned int b_tree_find(void *b_tree, void *key)
          // Iterate through the keys in the node
          for(int i = 0; i < (int)(curr_node->nkeys); ++i)
          {
-            printf("%c\n", *(curr_node->keys[i]));
+            printf("%c", *(curr_node->keys[i]));
+            printf("%c", *(curr_node->keys[i + 1]));
+            printf("%c\n", *(curr_node->keys[i + 1]));
             int compare = memcmp(key, curr_node->keys[i], mytree->key_size);
 
             printf("Compare is %d \n", compare);
