09fd81267f7882edb5c0edb592011980f0306845
Melnichenko Max <thrcn@TeachRaccoon>
2022-11-06 22:46:34 -0500

    3

--- src/b_tree.c
+++ src/b_tree.c
@@ -644,6 +644,7 @@ unsigned int insertion(B_Tree *mytree, Tree_Node *node_found, void *key, int rec
       {
          node_found->lbas[i] = lba1;
          node_found->lbas[i + 1] = lba2;
+         return 0;
       }
 
       node_found->nkeys = (unsigned char) ((int) (node_found ->nkeys) + 1);
